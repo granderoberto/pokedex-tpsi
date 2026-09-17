dda0e36f798aff69632c91c2c67bc1fa66dd2d2f
Roberto Grande <grande.roberto@iisgalvanimi.edu.it>
2026-09-17 10:44:58 +0200

    pagina con la lista delle categorie di pokemon e link alle rispettive route

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
 export class CategoriesComponent {
-
+  categories: string[] = ['normal', 'rock', 'ground'];
 }
