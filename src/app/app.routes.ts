f7d6e2e49178eb6aae6de8359515857fb7b74244
Roberto Grande <grande.roberto@iisgalvanimi.edu.it>
2026-09-17 10:35:46 +0200

    routing tra pagina categorie, lista pokemon per tipo e dettaglio pokemon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CategoriesComponent } from './categories/categories.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'type/:name', component: PokemonListComponent },
+  { path: 'pokemon/:name', component: PokemonDetailComponent },
+  { path: '', redirectTo: '/categories', pathMatch: 'full' }
+];
